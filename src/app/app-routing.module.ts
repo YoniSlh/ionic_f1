0526843b8c8d8cb7a0ecdc4cb75da018c7b65fd1
Yoni Slh <yonisel12@gmail.com>
2024-12-19 11:49:16 +0100

    fix de la tab-bar, ajout ionic camera en cours

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,38 +3,13 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () =>
-      import('./home/home.module').then((m) => m.HomePageModule),
-  },
-  {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    loadChildren: () =>
+      import('./tab/tab.module').then((m) => m.TabPageModule),
   },
   {
-    path: 'drivers',
-    loadChildren: () =>
-      import('./driver/driver.module').then((m) => m.DriverPageModule),
-  },
-  {
-    path: 'gestion/liste',
-    loadChildren: () =>
-      import('./gestion/liste/liste.module').then((m) => m.ListePageModule),
-  },
-  {
-    path: 'gestion/creation',
-    loadChildren: () =>
-      import('./gestion/creation/creation.module').then(
-        (m) => m.CreationPageModule
-      ),
-  },
-  {
-    path: 'gestion/modification/:id',
-    loadChildren: () =>
-      import('./gestion/modification/modification.module').then(
-        (m) => m.ModificationPageModule
-      ),
+    path: 'picture',
+    loadChildren: () => import('./picture/picture.module').then( m => m.PicturePageModule)
   }
 ];
 
---
+++ src/app/tab/tab-routing.module.ts
@@ -0,0 +1,65 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TabPage } from './tab.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TabPage,
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        loadChildren: () =>
+          import('../home/home.module').then((m) => m.HomePageModule),
+      },
+      {
+        path: 'drivers',
+        loadChildren: () =>
+          import('../driver/driver.module').then((m) => m.DriverPageModule),
+      },
+      {
+        path: 'gestion/liste',
+        loadChildren: () =>
+          import('../gestion/liste/liste.module').then(
+            (m) => m.ListePageModule
+          ),
+      },
+      {
+        path: 'gestion/creation',
+        loadChildren: () =>
+          import('../gestion/creation/creation.module').then(
+            (m) => m.CreationPageModule
+          ),
+      },
+      {
+        path: 'gestion/modification/:id',
+        loadChildren: () =>
+          import('../gestion/modification/modification.module').then(
+            (m) => m.ModificationPageModule
+          ),
+      },
+      {
+        path: 'picture',
+        loadChildren: () =>
+          import('../picture/picture.module').then((m) => m.PicturePageModule),
+      },
+      {
+        path: 'about',
+        loadChildren: () =>
+          import('../about/about.module').then((m) => m.AboutPageModule),
+      }
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabPageRoutingModule {}
