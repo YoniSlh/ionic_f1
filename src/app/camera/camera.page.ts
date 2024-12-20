f1416f568bfb8587d40c775c5fa33622ea100753
Yoni Slh <yonisel12@gmail.com>
2024-12-20 09:16:10 +0100

    Ajout caméra IONIC OK

--- src/app/camera/camera.page.ts
+++ src/app/camera/camera.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-camera',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./camera.page.scss'],
 })
 export class CameraPage implements OnInit {
-
+  public photos: picture[] = [];
   constructor() { }
 
   ngOnInit() {
   }
 
+  async addNewPhoto() {
+    const capture = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100
+    });
+
+    this.photos.unshift({
+      filepath: '',
+      webviewPath: capture.webPath
+    }); 
+  }
+
+  takePhoto(){
+    this.addNewPhoto();
+  }
+}
+
+export interface picture {
+  filepath: string;
+  webviewPath: string | undefined;
 }
